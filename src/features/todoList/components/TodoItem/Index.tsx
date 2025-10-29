ea3b2cc6808f84149c8a9efe0e449c9fbc427e8d
shreya pokhrel <pokhrelshreya944@gmail.com>
2025-10-29 21:53:04 +0545

    context menu

--- src/features/todoList/components/TodoItem/Index.tsx
+++ src/features/todoList/components/TodoItem/Index.tsx
@@ -1,7 +1,11 @@
 import React, { useState, useRef, useEffect } from "react";
 import { TextInput, ActionIcon, Tooltip } from "@mantine/core";
 import { IconGripVertical, IconTrash } from "@tabler/icons-react";
+import { Menu, Item, useContextMenu } from "react-contexify";
+import "react-contexify/ReactContexify.css";
 import type { TodoItemProps, EditableTextProps } from "../../../../types";
+
+const MENU_ID = "TODO_CONTEXT_MENU";
 
 const DragHandle: React.FC<{ dragHandleProps: any }> = ({
   dragHandleProps,
@@ -11,20 +15,26 @@ const DragHandle: React.FC<{ dragHandleProps: any }> = ({
   </span>
 );
 
-const EditableText: React.FC<EditableTextProps> = ({
+const EditableText: React.FC<EditableTextProps & { focus?: boolean }> = ({
   value,
   done,
   onChange,
+  focus,
 }) => {
   const [isOverflowed, setIsOverflowed] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     const el = inputRef.current;
-    if (el) {
-      setIsOverflowed(el.scrollWidth > el.clientWidth);
-    }
+    if (el) setIsOverflowed(el.scrollWidth > el.clientWidth);
   }, [value]);
+
+  useEffect(() => {
+    if (focus && inputRef.current) {
+      inputRef.current.focus();
+      inputRef.current.select();
+    }
+  }, [focus]);
 
   const textInput = (
     <TextInput
@@ -63,7 +73,6 @@ const EditableText: React.FC<EditableTextProps> = ({
   );
 };
 
-
 const TodoItem: React.FC<TodoItemProps> = ({
   todo,
   provided,
@@ -72,54 +81,69 @@ const TodoItem: React.FC<TodoItemProps> = ({
   onDelete,
 }) => {
   const [hovered, setHovered] = useState(false);
+  const [editing, setEditing] = useState(false);
+  const { show } = useContextMenu({ id: MENU_ID });
+
+  const handleContextMenu = (event: React.MouseEvent) => {
+    event.preventDefault();
+    show({ event, props: { todo } });
+  };
 
   return (
-    <div
-      ref={provided.innerRef}
-      {...provided.draggableProps}
-      style={{
-
-
-        ...provided.draggableProps.style,
-        background: snapshot.isDragging ? "#d1fae5" : "#fff",
-        border: "1px solid #ccc",
-        borderRadius: 6,
-        padding: "4px",
-        marginBottom: 4,
-        display: "flex",
-        alignItems: "center",
-        minHeight: 36,
-      }}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-    >
-      <DragHandle dragHandleProps={provided.dragHandleProps} />
-
-      <EditableText
-        value={todo.text}
-        done={todo.done ?? false}
-        onChange={onTextChange}
-      />
-
+    <>
       <div
+        ref={provided.innerRef}
+        {...provided.draggableProps}
+        onContextMenu={handleContextMenu}
         style={{
-          marginLeft: 8,
+          ...provided.draggableProps.style,
+          background: snapshot.isDragging ? "#d1fae5" : "#fff",
+          border: "1px solid #ccc",
+          borderRadius: 6,
+          padding: "4px",
+          marginBottom: 4,
           display: "flex",
           alignItems: "center",
-          opacity: hovered ? 1 : 0,
-          transition: "opacity 0.2s ease",
+          minHeight: 36,
+          cursor: "pointer",
         }}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
       >
-        <ActionIcon
-          color="red"
-          variant="subtle"
-          onClick={() => onDelete?.(todo.id)}
-          title="Delete todo"
+        <DragHandle dragHandleProps={provided.dragHandleProps} />
+
+        <EditableText
+          value={todo.text}
+          done={todo.done ?? false}
+          onChange={onTextChange}
+          focus={editing}
+        />
+
+        <div
+          style={{
+            marginLeft: 8,
+            display: "flex",
+            alignItems: "center",
+            opacity: hovered ? 1 : 0,
+            transition: "opacity 0.2s ease",
+          }}
         >
-          <IconTrash size={18} />
-        </ActionIcon>
+          <ActionIcon
+            color="red"
+            variant="subtle"
+            onClick={() => onDelete?.(todo.id)}
+            title="Delete todo"
+          >
+            <IconTrash size={18} />
+          </ActionIcon>
+        </div>
       </div>
-    </div>
+
+      <Menu id={MENU_ID}>
+        <Item onClick={() => setEditing(true)}>Edit</Item>
+        <Item onClick={({ props }) => onDelete?.(props.todo.id)}> Delete</Item>
+      </Menu>
+    </>
   );
 };
 
